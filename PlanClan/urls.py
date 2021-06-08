1f26e7afc0cedf9803c01caa426024b7be29c19e
afshinshiri <afshinshiri0211@gmail.com>
2021-06-08 14:36:58 +0430

    photo vid kamel

--- PlanClan/urls.py
+++ PlanClan/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about, name='about'),
     path('', include('blog.urls', namespace='blog')),
+    path('accounts/', include('accounts.urls', namespace='accounts'))
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.contrib.auth.decorators import login_required
+
+
 
 def post_list(request):
     object_list = Post.published.all()
-
-
-
     return render(request, 'blog/post_list.html', {'object_list': object_list})
+
+@login_required(login_url="/accounts/login")
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day, )
+    return render(request, 'blog/detail.html', {'post': post})
